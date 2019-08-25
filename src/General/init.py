3cb77614c40591c4a584ef6f2476db9171587491
itamarefr <48890839+itamarefr@users.noreply.github.com>
2019-08-25 20:31:21 +0300

    Fixed word2vec model download

--- src/General/init.py
+++ src/General/init.py
@@ -1,25 +1,27 @@
 import nltk
 import src.General.settings as settings
 import gensim.downloader as api
-from gensim.models import Word2Vec
+import pickle
 
 words_embeddings = None
 
-def init():
-  global words_embeddings
 
-  settings.init()
-  if bool(int(settings.config['preprocessing']['download'])):
-    download_nltk_datasets()
-    words_embeddings = api.load(settings.config['wordsembeddings']['modle_name'])
-    words_embeddings.save("../assets/word2vec.model")
-  else:
-    words_embeddings = Word2Vec.load("../assets/word2vec.model")
+def init():
+    global words_embeddings
+
+    settings.init()
+    if bool(int(settings.config['preprocessing']['download'])):
+        download_nltk_datasets()
+        words_embeddings = api.load(settings.config['wordsembeddings']['modle_name'])
+        pickle.dump(words_embeddings, open("../assets/word2vec.model", 'wb'))
+    else:
+        words_embeddings = pickle.load(open("../assets/word2vec.model", 'rb'))
+
 
 def download_nltk_datasets():
-  nltk.download('tagsets')
-  nltk.download('punkt')
-  nltk.download('averaged_perceptron_tagger')
-  nltk.download('maxent_ne_chunker')
-  nltk.download('words')
-  nltk.download('wordnet')
+    nltk.download('tagsets')
+    nltk.download('punkt')
+    nltk.download('averaged_perceptron_tagger')
+    nltk.download('maxent_ne_chunker')
+    nltk.download('words')
+    nltk.download('wordnet')
