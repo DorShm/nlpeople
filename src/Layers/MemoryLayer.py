1a036b7482d03a04bcfc515eef1f8a3e4e3475ba
Dor Shmaryahu <dshmaryahu@gmail.com>
2019-08-30 14:29:20 +0300

    Finished memory layer (no config yet..)

--- src/Layers/MemoryLayer.py
+++ src/Layers/MemoryLayer.py
@@ -5,19 +5,19 @@ from torchnlp.nn import Attention
 
 from src.General.Networks import OneLayerBRNN
 
-
-class MemoryLayer:
-  def __init__(self, H_Q, H_P, d, dropout=0.4):
+# TODO: add config to memory layer
+class MemoryLayer(nn.Module):
+  def __init__(self, config, d):
     super(MemoryLayer, self).__init__()
     self.q_transform = nn.RNN(input_size=(2 * d), hidden_size=128, nonlinearity='relu')
     self.p_transform = nn.RNN(input_size=(2 * d), hidden_size=128, nonlinearity='relu')
-    self.attention = Attention(2 * d)
+    self.attention = Attention(128, attention_type='dot')
     self.self_attention = Attention(4 * d)
-    self.bi_lstm = OneLayerBRNN()
+    self.bi_lstm = OneLayerBRNN(input_size=(8 * d), hidden_size=128, dropout=config['dropout'])
 
-    self.dropout = nn.Dropout(dropout)
+    self.dropout = nn.Dropout(float(config['dropout']))
 
-  def run(self, H_Q, H_P):
+  def forward(self, H_Q, H_P):
     C = self.phase_one(H_Q, H_P)
     U_P = self.phase_two(H_Q, H_P, C)
     M_input = self.phase_three(U_P)
@@ -26,30 +26,39 @@ class MemoryLayer:
     return M
 
   def phase_one(self, H_Q, H_P):
-    H_Q_hat = self.q_transform(H_Q)
-    H_P_hat = self.p_transform(H_P)
+    out_q, H_Q_hat = self.q_transform(H_Q)
+    out_h, H_P_hat = self.p_transform(H_P)
 
-    # TODO: Apply dropout
-    att_output, _ = self.attention(H_Q_hat, H_P_hat)
-    C = self.dropout(att_output)
+    att_output, att_weights = self.attention(H_Q_hat, H_P_hat)
+    C = self.dropout(att_weights)
 
     return C
 
   def phase_two(self, H_Q, H_P, C):
-    U_P = torch.cat(H_P, torch.dot(H_Q, C))
+    dot_product = torch.bmm(torch.transpose(H_Q, 1, 2), C)
+    U_P = torch.cat((H_P, torch.transpose(dot_product, 1, 2)), 2)
 
     return U_P
 
   def phase_three(self, U_P):
-    self_att_output = self.self_attention(U_P, U_P)
-    # TODO: Apply drop_diag (whatever it means...)
-    U_P_dropped = U_P
+    self_att_output, self_att_weights = self.self_attention(U_P, U_P)
 
-    U_P_hat = torch.dot(U_P, U_P_dropped)
+    U_P_dropped = self.drop_diag(self_att_weights)
 
-    return torch.cat(U_P, U_P_hat)
+    U_P_transposed = torch.transpose(U_P, 1, 2)
+
+    U_P_hat = torch.bmm(U_P_transposed, U_P_dropped)
+
+    return torch.cat((U_P, torch.transpose(U_P_hat, 1, 2)), 2)
 
   def phase_four(self, memory_input):
     M = self.bi_lstm(memory_input)
 
     return M
+
+  def drop_diag(self, mat):
+    mask = torch.eye(mat.shape[1], mat.shape[2])
+
+    mat[:].masked_fill_(mask.bool(), 0)
+
+    return mat
