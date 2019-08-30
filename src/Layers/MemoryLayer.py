a4c0d1ad012e270e96a4a41e2861001a3b2345ec
Dor Shmaryahu <dshmaryahu@gmail.com>
2019-08-30 11:19:53 +0300

    Added (1)

--- src/Layers/MemoryLayer.py
+++ src/Layers/MemoryLayer.py
@@ -1,9 +1,14 @@
 import torch
 from torch import nn
+from torchnlp.nn import Attention
 
-class MemoryLayer(nn.Module):
-  def __init__(self):
+class MemoryLayer:
+  def __init__(self, H_Q, H_P, d):
     super(MemoryLayer, self).__init__()
+    self.q_transform = nn.RNN(input_size=(2 * d), hidden_size=128, nonlinearity='relu')
+    self.p_transform = nn.RNN(input_size=(2 * d), hidden_size=128, nonlinearity='relu')
+    self.attention = Attention(2 * d)
 
-  def forward(self, *input, **kwargs):
-    pass
+  def phase_one(self, H_Q, H_P):
+    H_Q_hat = self.q_transform(H_Q)
+    H_P_hat = self.p_transform(H_P)
