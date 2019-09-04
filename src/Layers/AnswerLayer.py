ce67650402bcfb3646328ca90b821e39ece5fda6
itamarefr <48890839+itamarefr@users.noreply.github.com>
2019-09-04 22:10:31 +0300

    Created the first step of the answer layer

--- src/Layers/AnswerLayer.py
+++ src/Layers/AnswerLayer.py
@@ -1,16 +1,25 @@
+import torch
 from torch import nn
 from torch.nn import functional as F
 
 
 class AnswerLayer(nn.Module):
-    def __init__(self):
+    def __init__(self, turns):
         super(AnswerLayer, self).__init__()
-        self.gru = None
-        self.turns = None
-        self.predict_begin_network = None
-        self.predict_end_network = None
-        self.softmax = F.softmax
+        self.gru = nn.GRU()
+        self.turns = turns
+        self.predict_begin_network = nn.Bilinear()
+        self.predict_end_network = nn.Bilinear()
+        self.projection = nn.Linear()
 
-    def forward(self, x, h_0):
+    def forward(self, M, s_t):
+        start_score_list = []
+        end_score_list = []
         for turn in range(self.turns):
-            start = self.predict_begin_network(x, h_0)
+            start_vector = self.predict_begin_network(M, s_t)
+            end_vector = self.predict_end_network(M, s_t + start_vector)
+            x_t = F.softmax(torch.bmm(start_vector, M))
+            x_t = torch.bmm(x_t, M)
+            s_t = self.gru(x_t, s_t)
+            start_score_list.append(start_vector)
+            end_score_list.append(end_vector)
