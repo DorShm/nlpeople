c36befec584cc8a3d9317383c6c7820e1e2db754
itamarefr <48890839+itamarefr@users.noreply.github.com>
2019-09-06 16:12:37 +0300

    Started to create the epoches stage and fitted the squad model to work with the qa_model and the main function.

--- src/SQuADModel.py
+++ src/SQuADModel.py
@@ -1,4 +1,5 @@
-import torch
+import ast
+
 from torch import Tensor, optim as optimizer
 from torch.autograd import Variable
 from torch.nn import utils, functional as F
@@ -10,28 +11,25 @@ from src.General.QAModule import QAModule
 # noinspection PyArgumentList
 class SQuADModel:
   def __init__(self, words_embeddings, config):
+    self.config = config
     self.qa_module: QAModule = QAModule(words_embeddings, config)
-    model_config = config['squad_model']
-    self.cuda_on = bool(model_config['cuda_on'])
+    self.cuda_on = ast.literal_eval(self.config['cuda_on'])
     self.model_loss = ModelLoss()
-
-    # TODO: Add optimizer parameters to config learning_rate=0.002
-    learning_rate = 0.002
     parameters = [parameter for parameter in self.qa_module.parameters() if parameter.requires_grad]
     # TODO: Consider adding optimizer lr_scheduler
-    self.optimizer = optimizer.Adamax(parameters, learning_rate)
+    self.optimizer = optimizer.Adamax(parameters, float(self.config['learning_rate']))
 
 
-  def update(self, instance):
+  def update(self, paragrapth, question):
     self.qa_module.train()
 
-    start_tensor = self.get_tensor(instance['start'])
-    end_tensor: Tensor = self.get_tensor(instance['end'])
+    start_tensor = self.get_tensor(question['answer_start'])
+    end_tensor: Tensor = self.get_tensor(question['answer_end'])
 
     start_label = Variable(start_tensor)
     end_label = Variable(end_tensor)
 
-    start, end, prediction = self.qa_module(instance)
+    start, end, prediction = self.qa_module(paragrapth, question)
     # noinspection PyTypeChecker
     loss = F.cross_entropy(start, start_label) + F.cross_entropy(end, end_label)
     self.model_loss.calculate_loss(loss)
