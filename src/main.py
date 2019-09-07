70645e85779e7ef7c4eaff0ab6706f625e2a1ece
itamarefr <48890839+itamarefr@users.noreply.github.com>
2019-09-07 14:12:31 +0300

    Added more log comment and progress bars to main and preprocessing.

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import ast
 import json
 
+from tqdm import tqdm
 from src.General import settings, init
 from src.Preprocess import preprocess
 from src.SQuADModel import SQuADModel
@@ -9,17 +10,18 @@ from src.SQuADModel import SQuADModel
 def main():
     logger = init.init()
     if ast.literal_eval(settings.config['preprocessing']['preprocess']):
-        preprocess.preprocess(ast.literal_eval(settings.config['preprocessing']['save_preprocess_data']))
-
+        preprocess.preprocess(logger, ast.literal_eval(settings.config['preprocessing']['save_preprocess_data']))
+    logger.info(f"Loading preprocessed data from {settings.config['general']['data_file']}")
     with open(settings.config['general']['data_file'], 'r') as f:
       data = json.load(f)['data']
     squad_model = SQuADModel(init.words_embeddings, settings.config, logger)
     logger.info('Starting to train the network')
     for epoch in range(int(settings.config['general']['epoches'])):
         logger.info(f'Starting epoch {epoch}')
-        for paragraph in data:
+        for paragraph in tqdm(data, total=len(data)):
             for question in paragraph['qas']:
                 squad_model.update(paragraph, question)
+
 
 
 if __name__ == '__main__':
