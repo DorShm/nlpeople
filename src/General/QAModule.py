Fixed model softmax
Added test_model options

src/General/QAModule.py
@@ -1,5 +1,5 @@
 import json
-from torch import nn
+from torch import nn, Tensor
 from Layers.MemoryLayer import MemoryLayer
 from Layers.AnswerLayer import AnswerLayer
 from Encoders.GermanEnglishCoVe import GermanEnglishCoVe
@@ -47,7 +47,7 @@ class QAModule(nn.Module):
     self.linear_self_attention.cuda()
     self.answer_layer.cuda()
 
-  def forward(self, sentence, question):
+  def forward(self, sentence, question) -> (Tensor, Tensor):
     # Lexicon Layer
     paragraph_vector, question_vector, paragraph_emb, question_emb = self.lexicon_encoder(sentence, question)
 

src/Layers/AnswerLayer.py
@@ -19,16 +19,18 @@ class AnswerLayer(nn.Module):
         for turn in range(self.turns):
             # Compute P_t^begin
             # P_t_begin <=> start_vector (Softmax might better be after predict_end_network usage
-            start_vector = F.softmax(self.predict_begin_network(M, s_t), 1)
+            start_temp = self.predict_begin_network(M, s_t)
 
             # Compute P_t^end
             # P_t_end <=> end_vector
             # TODO: Change this shitty name "square braces"
-            square_braces = s_t + torch.bmm(start_vector.unsqueeze(1), M).squeeze(1)
+            square_braces = s_t + torch.bmm(start_temp.unsqueeze(1), M).squeeze(1)
+
+            start_vector = F.softmax(start_temp, 1)
             end_vector = F.softmax(self.predict_end_network(M, square_braces), 1)
 
             # Compute β = s_t*W_5*M != s_(t+1)*W_6*M = P_t^begin
-            β = start_vector
+            β = start_temp
             # Compute x_(t+1) = β*M
             x_t = torch.bmm(β.unsqueeze(1), M).squeeze(1)
 

src/SQuADModel.py
@@ -1,6 +1,8 @@
 import ast
+import math
 
 import torch
+import numpy as np
 import logging
 from torch import Tensor, optim as optimizer
 from torch.autograd import Variable
@@ -61,23 +63,62 @@ class SQuADModel:
     return tensor.cuda() if self.cuda_on else tensor
 
   # TODO: Return model result
-  def predict(self):
+  def predict1(self, paragraph, question):
     self.qa_module.eval()
-    pass
+
+    start, end = self.qa_module(paragraph, question)
+    pos_end = self.position_encoding(start.size(1), start.size(1))
+    scores = torch.ger(start[0], end[0])
+    scores = scores * pos_end
+    scores.triu_()
+    top_k = 1
+    scores = scores.detach().cpu().numpy()
+    best_idx = np.argpartition(scores, -top_k, axis=None)[-top_k]
+    best_score = np.partition(scores, -top_k, axis=None)[-top_k]
+    s_idx, e_idx = np.unravel_index(best_idx, scores.shape)
+    answer_start, answer_end = int(question['answer_start']), int(question['answer_end'])
+
+    return s_idx, e_idx
+
+  def position_encoding(self, m, threshold=5):
+    encoding = np.ones((m, m), dtype=np.float32)
+    for i in range(m):
+      for j in range(i, m):
+        if j - i > threshold:
+          encoding[i][j] = float(1.0 / math.log(j - i + 1))
+    return torch.from_numpy(encoding).cuda()
+
+  def predict(self, paragraph, question):
+    self.qa_module.eval()
+
+    start_prediction, end_prediction = self.qa_module(paragraph, question)
+
+    start = np.argmax(start_prediction.detach().cpu().numpy())
+    end = np.argmax(end_prediction.detach().cpu().numpy())
+
+    return start, end
 
   # TODO: Necessary to compute accuracy
-  def eval(self):
-    pass
+  def eval(self, predictions, labels) -> float:
+    predictions = np.array(predictions)
+    labels = np.array(labels)
 
-  def load(self, epoch):
+    exact_match = (predictions == labels).all(axis=1).mean()
+    half_match = (predictions == labels).any(axis=1).mean()
+
+    return exact_match, half_match
+
+  def load(self, epoch='best'):
     """
     Load trained model state dict from file
     :param epoch: epoch number
     """
+    self.logger.info(f'Using model loaded from {self.config["squad_model_path"].format(epoch)}')
+
     state_dict = torch.load(self.config["squad_model_path"].format(epoch))
     self.qa_module.load_state_dict(state_dict)
 
-  def save(self, epoch):
+  def save(self, epoch='best'):
     """
     Save trained model state dict
     :param epoch: epoch number